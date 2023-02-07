267cfe7a23ab6c48a9749fa73f34faed998f96b5
Sahadsawad Chailuan <possible.pack@gmail.com>
2023-02-08 02:06:40 +0700

    first commit

---
+++ src/pages/Medicine.vue
@@ -0,0 +1,70 @@
+<!-- eslint-disable no-mixed-spaces-and-tabs -->
+<template>
+<section class="content bg">
+    <div class="mx-5 rounded h-100 d-flex align-items-center justify-content-center">
+      <div class="mt-3 container-fluid px-5 card">
+          <div class="card-body">
+              <h1 class="mt-4 mb-4">ข้อมูลยา</h1>
+              <div class="mb-4">
+                  <div>
+						<div class="d-flex justify-content-end">
+							<button class="mb-4 btn bg-primary"><a href="addmedicine.html" style="text-decoration: none" class="text-white"><i class='bx bx-plus-medical'></i> เพิ่มยา</a></button>
+						</div>
+						<DataTable :data="data" class="display">
+    					    <thead>
+    					        <tr>
+    					            <th style="width: 5%;">รหัส</th>
+                    	              <th style="width: 10%;">ชื่อยา</th>
+                    	              <th style="width: 20%;">รูปยา</th>
+                    	              <th style="width: 20%;">รายละเอียด</th>
+                    	              <th style="width: 20%;">วิธีการใช้ยา</th>
+                    	              <th style="width: 13%;">
+									  </th>
+    					        </tr>
+    					    </thead>
+    					</DataTable>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </div>
+</section>
+</template>
+
+<script>
+
+
+// // Your web app's Firebase configuration
+// // eslint-disable-next-line no-unused-vars
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD3cuKqdUz7--QTLZ7gZNOrAvRi0dxyhDc",
+//   authDomain: "dyeac-dev.firebaseapp.com",
+//   projectId: "dyeac-dev",
+//   storageBucket: "dyeac-dev.appspot.com",
+//   messagingSenderId: "675710346530",
+//   appId: "1:675710346530:web:ddf3b0114948ae5668f17b"
+// };
+
+// // Initialize Firebase
+// // const app = initializeApp(firebaseConfig);
+
+
+export default {
+	name: 'App',
+    components: {
+	},
+    data() {
+      return {}
+    },
+	methods() {
+		
+	},	
+	mounted (){
+	},
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/Registerpatient.vue
@@ -0,0 +1,65 @@
+<template> 
+    <div class="content bg">
+        <div class="mx-5 rounded h-100 d-flex align-items-center justify-content-center">
+            <div class="mt-3 container-fluid px-5 card">
+                <div class="card-body">
+                    <h2 class="mt-5 mb-4 d-flex justify-content-center">ลงทะเบียนผู้ป่วยใหม่</h2>
+                    <form class="mt-4 mb-4" action="">
+                            <div class="row ">
+                                <div class="col-6">
+                                    <label class="form-label">ชื่อ</label>
+                                    <input class="form-control" type="text" id="name" placeholder="กรุณาป้อนชื่อ" required>
+                                </div>
+                                <div class="col-6">
+                                    <label class="form-label">นามสกุล</label>
+                                    <input class="form-control" type="text" id="surname" placeholder="กรุณาป้อนนามสกุล" required>
+                                </div>
+                                <div class="col-6">
+                                    <label class="form-label">เบอร์โทรศัพท์</label>
+                                    <input class="form-control" type="tel" id="phoneNO" placeholder="กรุณาป้อนเบอร์โทรศัพท์" required>
+                                </div>
+                                <div class="col-6">
+                                    <label class="form-label">อีเมล</label>
+                                    <input class="form-control" type="email" id="email" placeholder="กรุณาป้อนอีเมล" >
+                                </div>
+                                <div class="col-6">
+                                    <label class="form-label">รหัสผ่าน</label>
+                                    <input class="form-control" type="password" id="password" placeholder="กรุณาป้อนรหัสผ่าน" required>
+                                </div>
+                                <br>
+                                <div class="col-6">
+                                    <label class="form-label">ยืนยันรหัสผ่าน</label>
+                                    <input class="form-control" type="password" id="repassword" placeholder="กรุณาป้อนรหัสผ่าน" >
+                                </div>
+                                <br>
+                                <div class="col-6">
+                                    <label class="form-label d-flex justify-content-start">แพทย์ผู้ดูแล</label>
+                                    <input class="form-control disabled" type="text" id="doctorname" required readonly>
+                                </div>
+                                <div class="col-6">
+                                </div>
+                                <div class="mt-4">
+                                    <button type="submit" class="btn bg-success text-white" name="submitData" id="submitData">บันทึกข้อมูล</button>
+                                </div>
+                            </div>
+                    </form>
+                </div>  
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.card{
+  border-radius: 25px;
+}
+
+</style>
