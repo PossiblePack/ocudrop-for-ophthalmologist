d8e50342316d334b4e8a40190b9da0f5a309f1d4
Sahadsawad Chailuan <possible.pack@gmail.com>
2023-04-08 03:46:49 +0700

    delete firebase token

--- src/firebase.js
+++ src/firebase.js
@@ -5,28 +5,8 @@ import { firebase, initializeApp, applicationDefault, cert } from 'firebase/app'
 import { query, where, updateDoc, addDoc, collection, getDocs, getDoc, getFirestore, deleteDoc, doc } from "firebase/firestore";
 import { getStorage, ref as sRef, uploadBytesResumable, getDownloadURL} from "firebase/storage"
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import {firebaseConfig} from "./firebaseConfig"
 
-// // Dyeac-Dev
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD3cuKqdUz7--QTLZ7gZNOrAvRi0dxyhDc",
-//   authDomain: "dyeac-dev.firebaseapp.com",
-//   projectId: "dyeac-dev",
-//   storageBucket: "dyeac-dev.appspot.com",
-//   messagingSenderId: "675710346530",
-//   appId: "1:675710346530:web:ddf3b0114948ae5668f17b"
-// };
-
-// Dyeac
-const firebaseConfig = {
-  apiKey: "AIzaSyAoZzqNTkmRw3ycCZlWRODV3pvpeF_Z-eg",
-  authDomain: "dyeac-8fc86.firebaseapp.com",
-  databaseURL: "https://dyeac-8fc86-default-rtdb.firebaseio.com",
-  projectId: "dyeac-8fc86",
-  storageBucket: "dyeac-8fc86.appspot.com",
-  messagingSenderId: "255357549075",
-  appId: "1:255357549075:web:826dfcba6022e3cdfe1de6",
-  measurementId: "G-G0XDWNWV6X"
-};
   
   // Initialize Firebase
 const app = initializeApp(firebaseConfig);
